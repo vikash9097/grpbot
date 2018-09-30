752e4d170048f3cac80d01da6eedfdf964ebd576
unknown <gamelovr695@gmail.com>
2018-09-30 13:09:03 +0530

    Fix get link

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -8,39 +8,41 @@ from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
 
-import telegram
 import tg_bot.modules.sql.users_sql as sql
+import random, re
 from tg_bot import dispatcher, OWNER_ID, LOGGER
 from tg_bot.modules.helper_funcs.filters import CustomFilters
-from tg_bot.modules.disable import DisableAbleCommandHandler
 
-USERS_GROUP=4
-                                                                                                                                                                                                                                                                               
-@run_async                                                                                                                                                                                                                                                                     
-def quickscope(bot: Bot, update: Update, args: List[int]):                                                                                                                                                                                                                     
-    if args:                                                                                                                                                                                                                                                                   
-        chat_id = str(args[1])                                                                                                                                                                                                                                                 
-        to_kick = str(args[0])                                                                                                                                                                                                                                                 
-    else:                                                                                                                                                                                                                                                                      
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")                                                                                                                                                                                   
-    try:                                                                                                                                                                                                                                                                       
-        bot.kick_chat_member(chat_id, to_kick)                                                                                                                                                                                                                                 
-        update.effective_message.reply_text("Attempted banning " + to_kick + " from" + chat_id)                                                                                                                                                                                
-    except BadRequest as excp:                                                                                                                                                                                                                                                 
-        update.effective_message.reply_text(excp.message + " " + to_kick)                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                               
-@run_async                                                                                                                                                                                                                                                                     
-def quickunban(bot: Bot, update: Update, args: List[int]):                                                                                                                                                                                                                     
-    if args:                                                                                                                                                                                                                                                                   
-        chat_id = str(args[1])                                                                                                                                                                                                                                                 
-        to_kick = str(args[0])                                                                                                                                                                                                                                                 
-    else:                                                                                                                                                                                                                                                                      
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")                                                                                                                                                                                   
-    try:                                                                                                                                                                                                                                                                       
-        bot.unban_chat_member(chat_id, to_kick)                                                                                                                                                                                                                                
+USERS_GROUP = 4
+
+
+@run_async
+def quickscope(bot: Bot, update: Update, args: List[int]):
+    if args:
+        chat_id = str(args[1])
+        to_kick = str(args[0])
+    else:
+        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
+    try:
+        bot.kick_chat_member(chat_id, to_kick)
+        update.effective_message.reply_text("Attempted banning " + to_kick + " from" + chat_id)
+    except BadRequest as excp:
+        update.effective_message.reply_text(excp.message + " " + to_kick)
+
+
+@run_async
+def quickunban(bot: Bot, update: Update, args: List[int]):
+    if args:
+        chat_id = str(args[1])
+        to_kick = str(args[0])
+    else:
+        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
+    try:
+        bot.unban_chat_member(chat_id, to_kick)
         update.effective_message.reply_text("Attempted unbanning " + to_kick + " from" + chat_id)
     except BadRequest as excp:
         update.effective_message.reply_text(excp.message + " " + to_kick)
+
 
 @run_async
 def banall(bot: Bot, update: Update, args: List[int]):
@@ -59,13 +61,6 @@ def banall(bot: Bot, update: Update, args: List[int]):
             update.effective_message.reply_text(excp.message + " " + str(mems.user))
             continue
 
-@bot_admin
-def leavechat(bot: Bot, update: Update, args: List[int]):
-    if args:
-        chat_id = int(args[0])
-        bot.leaveChat(chat_id)
-    else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat")
 
 @run_async
 def snipe(bot: Bot, update: Update, args: List[str]):
@@ -83,20 +78,66 @@ def snipe(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")
 
 
-__help__ = ""  # no help string
+@run_async
+@bot_admin
+def getlink(bot: Bot, update: Update, args: List[int]):
+    message = update.effective_message
+    if args:
+        pattern = re.compile(r'-\d+')
+    else:
+        message.reply_text("You don't seem to be referring to any chats.")
+    links = "Invite link(s):\n"
+    for chat_id in pattern.findall(message.text):
+        try:
+            chat = bot.getChat(chat_id)
+            bot_member = chat.get_member(bot.id)
+            if bot_member.can_invite_users:
+                invitelink = bot.exportChatInviteLink(chat_id)
+                links += str(chat_id) + ":\n" + invitelink + "\n"
+            else:
+                links += str(chat_id) + ":\nI don't have access to the invite link." + "\n"
+        except BadRequest as excp:
+                links += str(chat_id) + ":\n" + excp.message + "\n"
+        except TelegramError as excp:
+                links += str(chat_id) + ":\n" + excp.message + "\n"
 
+    message.reply_text(links)
+
+@bot_admin
+def leavechat(bot: Bot, update: Update, args: List[int]):
+    if args:
+        chat_id = int(args[0])
+        bot.leaveChat(chat_id)
+    else:
+        update.effective_message.reply_text("You don't seem to be referring to a chat")
+
+__help__ = """  
+**Owner only:**
+- /getlink **chatid**: Get the invite link for a specific chat.
+- /banall: Ban all members from a chat
+- /leavechat **chatid** : leave a chat
+**Sudo/owner only:**
+- /quickscope **chatid** **userid**: Ban user from chat.
+- /quickunban **chatid** **userid**: Unban user from chat.
+- /snipe **chatid** **string**: Make me send a message to a specific chat.
+- /rban **chatid** **userid** remotely ban a user from a chat
+- /runban **chatid** **userid** remotely unban a user from a chat
+- /stats: check bot's stats
+- /chatlist: get chatlist 
+- /gbanlist: get gbanned users list
+"""
 __mod_name__ = "Special"
 
-SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args = True, filters=CustomFilters.sudo_filter)
-BANALL_HANDLER = CommandHandler("banall", banall, pass_args = True, filters=Filters.user(OWNER_ID))
-QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args = True, filters=CustomFilters.sudo_filter)
-QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args = True, filters=CustomFilters.sudo_filter)
+SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
+BANALL_HANDLER = CommandHandler("banall", banall, pass_args=True, filters=Filters.user(OWNER_ID))
+QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args=True, filters=CustomFilters.sudo_filter)
+QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args=True, filters=CustomFilters.sudo_filter)
+GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
 LEAVECHAT_HANDLER = CommandHandler("leavechat", leavechat, pass_args=True, filters=Filters.user(OWNER_ID))
-
 
 dispatcher.add_handler(SNIPE_HANDLER)
 dispatcher.add_handler(BANALL_HANDLER)
 dispatcher.add_handler(QUICKSCOPE_HANDLER)
 dispatcher.add_handler(QUICKUNBAN_HANDLER)
+dispatcher.add_handler(GETLINK_HANDLER)
 dispatcher.add_handler(LEAVECHAT_HANDLER)
-
